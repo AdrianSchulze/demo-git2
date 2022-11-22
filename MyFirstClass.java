2654e1419d49a5517b62708ce0108432ac360d44
AdrianSchulze <adrian@sei-sicherer.de>
2022-11-22 16:53:35 +0100

    init: BIG BANG

--- MyFirstClass.java
+++ MyFirstClass.java
@@ -2,5 +2,6 @@ public class MyFirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Neue Fischeeeeeee");
+        System.out.println("bababa");
     }
 }
